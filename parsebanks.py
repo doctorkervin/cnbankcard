3f89c0c32aff7dd66391768c8ead9089c35849cb
kervin <doctorkervin@gmail.com>
2019-12-28 11:19:49 +0800

    python2 convert to python3

--- parsebanks.py
+++ parsebanks.py
@@ -1,16 +1,19 @@
 from bs4 import BeautifulSoup
-import urllib2
+import urllib.error
+import urllib.request
 import json
+
+from pip._vendor.appdirs import system
 
 ALI_BANKS = 'http://ab.alipay.com/i/yinhang.htm'
 
 try:
-    response = urllib2.urlopen(ALI_BANKS)
-except urllib2.HTTPError, e:
-    print "HTTP Error %s : %s" % (e.code, e.reason)
+    response = urllib.request.urlopen(ALI_BANKS)
+except urllib.error.HTTPError as e:
+    print("HTTP Error %s : %s" % (e.code, e.reason))
     system.exit(1)
-except urllib2.URLError, e:
-    print "Unable to connect the remote server."
+except urllib.error.URLError as e:
+    print("Unable to connect the remote server.")
     system.exit(1)
 
 content = response.read()
@@ -21,7 +24,7 @@ spans = soup.find_all("span", class_="icon")
 # for span in spans:
 #     banks[span['class'][1]] = span.text
 
-banks = { span['class'][1] : span.text for span in spans }
+banks = {span['class'][1]: span.text for span in spans}
 
-print json.dumps(banks, ensure_ascii=False, indent=2)
-print "total : %d" % len(banks)
+print(json.dumps(banks, ensure_ascii=False, indent=2))
+print("total : %d" % len(banks))
